d5b0bcbc578ab36355ade04c975673b048938ed4
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-03-20 16:40:30 +0530

    added sidebar with search list

--- routes/percap.js
+++ routes/percap.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var json = require('../public/data/percap.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('percap');
+  var data={
+    "json" : json,
+    "id" : req.query.id || 0
+  }
+  res.render('percap',{"data" : data});
 });
 
 module.exports = router;
